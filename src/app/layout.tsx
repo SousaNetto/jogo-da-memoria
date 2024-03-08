ac57a547d29407f6bc3a9e0cc4953d539b6ff607
José Neto <joseneto1500@gmail.com>
2024-03-08 15:09:53 -0300

    add: metadata and opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,22 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import ogImage from '@/app/opengraph-image.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Dev memory',
-  description: 'criado por Netto',
+  description:
+    'jogo da memória com o tema de programação, desafie seus amigos e divirtam-se',
+  metadataBase: new URL('https://jogo-da-memoria-silk.vercel.app/'),
+  openGraph: {
+    images: {
+      url: ogImage.src,
+      width: ogImage.width,
+      height: ogImage.height,
+    },
+  },
 }
 
 export default function RootLayout({
